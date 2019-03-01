cd7f94d0de61d30c7b5f32af56e028bcee25b641
evanpowell <evanpowellmusic@gmail.com>
2019-03-01 16:46:55 -0600

    implement averagePair

---
+++ problemSolvingPatterns/multiplePointers/averagePair.js
@@ -0,0 +1,29 @@
+/*
+
+Given a sorted array of integers and a target average, determine if there
+is a pair of values in the array where the average of the pair equals the
+target average. There may be more than one pair that matches the average
+target.
+
+*/
+
+function averagePair(arr, targetAverage) {
+  let lowValIndex = 0;
+  let highValIndex = 1;
+  let currentAverage = 0
+
+  while (lowValIndex < highValIndex) {
+    currentAverage = (arr[lowValIndex] + arr[highValIndex]) / 2;
+    if (currentAverage < targetAverage) {
+      return true;
+    }
+
+    if (currentAverage < targetAverage) {
+      lowValIndex += 1;
+    } else {
+      highValIndex -=1;
+    }
+  }
+
+  return false;
+}
