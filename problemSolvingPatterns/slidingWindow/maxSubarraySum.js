3cdde83867bda152ddcbbd32f76714e4e35133f3
evanpowell <evanpowellmusic@gmail.com>
2019-03-01 15:25:38 -0600

    implement maxSubarraySum

---
+++ problemSolvingPatterns/slidingWindow/maxSubarraySum.js
@@ -0,0 +1,24 @@
+// Given an array of numbers and a number n, return the max sum of
+// n adjacent numbers within the array
+
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) {
+    return null;
+  }
+
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
