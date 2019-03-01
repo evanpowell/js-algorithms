14e8cf5738db0a72196c50b8e92b6ee403d487f0
evanpowell <evanpowellmusic@gmail.com>
2019-03-01 14:08:37 -0600

    implement validAnagram frequency counter solution

---
+++ problemSolvingPatterns/frequencyCounter/validAnagram.js
@@ -0,0 +1,27 @@
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  
+  const charFrequencyBalanceCounter = {};
+  
+  for (let char of str1) {
+    charFrequencyBalanceCounter[char] = ++charFrequencyBalanceCounter[char] || 1;
+  }
+  
+  for (let char of str2) {
+    if (!(char in charFrequencyBalanceCounter)) {
+      return false;
+    }
+
+    charFrequencyBalanceCounter[char] -= 1;
+  }
+
+  for (let frequencyBalance of Object.values(charFrequencyBalanceCounter)) {
+    if (frequencyBalance !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
