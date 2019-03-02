3983dbc4803283ef1ebda4ebfcaf75f33c5d0d91
evanpowell <evanpowellmusic@gmail.com>
2019-03-02 14:38:47 -0600

    implement productOfArray

---
+++ recursion/productOfArray.js
@@ -0,0 +1,14 @@
+/*
+
+Write a function which takes in an array of numbers
+and returns the products of them all.
+
+*/
+
+function productOfArray(arr) {
+  if (arr.length === 0) {
+    return 1;
+  }
+
+  return arr[0] * productOfArray(arr.slice(1));
+}
